83528d0afe3b5edb05c54ca1c4515a933f020a80
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2023-12-23 15:07:29 +0500

    banner-changes

---
+++ Components/Sevices/Services.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='py-8' >
+        <button className='primaryBtn' >Contact Us</button>
+    </div>
+  )
+}
+
+export default Services
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import { cn } from "@/lib/utils"
 import Bananer from "@/Components/Banner/Banner";
 import TopBar from "@/Components/TopBar/TopBar";
+<<<<<<< Updated upstream
 import NavBar from "@/Components/NavBar/NavBar";
 import Members from "@/Components/Members/Members";
+=======
+import Services from "@/Components/Sevices/Services";
+>>>>>>> Stashed changes
 
 export default function Home() {
   return (
     <div>
       <TopBar />
       <Bananer />
+<<<<<<< Updated upstream
       <Members />
+=======
+      <Services />
+>>>>>>> Stashed changes
     </div>
   )
 }
